d2c708d9474e808459ae42d5cf4f7f71c0374073
Mike Ash <mikeash@apple.com>
2022-11-14 14:25:11 -0500

    Commentate and clean up

--- test/Runtime/Inputs/custom_rr_abi_utilities.h
+++ test/Runtime/Inputs/custom_rr_abi_utilities.h
@@ -1,5 +1,7 @@
 #define NUM_REGS 30
 
+// Apply `macro` to "all" registers. Skip x18 since it's reserved, and x30 since
+// it's the link register.
 #define ALL_REGS(macro) \
   macro( 0) \
   macro( 1) \
@@ -31,6 +33,10 @@
   macro(28) \
   macro(29)
 
+// Apply `macro` with the given parameters to all registers that have
+// specialized entrypoints. That's the same as ALL_REGS, minus x0 (the standard
+// entrypoint covers that), x16/x17 (temporary registers used as linker glue),
+// and x29 (the link register).
 #define FUNCTION_REGS(macro, ...) \
   macro( 1, __VA_ARGS__) \
   macro( 2, __VA_ARGS__) \
@@ -58,28 +64,41 @@
   macro(27, __VA_ARGS__) \
   macro(28, __VA_ARGS__)
 
+// Apply `macro` to each function that gets specialized entrypoints. Also pass
+// 1 if the function is a retain variant, and 0 if it's a release variant.
 #define ALL_FUNCTIONS(macro) \
   macro(swift_retain, 1) \
   macro(swift_release, 0) \
+  macro(swift_bridgeObjectRetain, 1) \
+  macro(swift_bridgeObjectRelease, 0)
 
+// Emit declarations for variables called xN stored in xN, initialized with
+// regs[N].
 #define PASS_REGS_HELPER(num) \
   register void *x ## num asm ("x" #num) = regs[num];
 #define PASS_REGS ALL_REGS(PASS_REGS_HELPER)
 
+// Emit an entry in an asm inputs list containing "r" (xN).
 #define REG_INPUTS_HELPER(num) \
   "r" (x ## num),
 #define REG_INPUTS ALL_REGS(REG_INPUTS_HELPER)
 
+// Make a function called call_function_xN that calls function_xN with registers
+// set to the contents of the given registers array.
 #define MAKE_CALL_FUNC(reg, func) \
   static inline void call_ ## func ## _x ## reg(void **regs) { \
     PASS_REGS \
     asm("bl _" #func "_x" #reg: : REG_INPUTS "i" (0)); \
   }
 
+// Make a call_function_xN for each specialized function and register.
 #define MAKE_ALL_CALL_FUNCS(function, isRetain) \
   FUNCTION_REGS(MAKE_CALL_FUNC, function)
 ALL_FUNCTIONS(MAKE_ALL_CALL_FUNCS)
 
+// Call `call` with each call_function_xN function created above, as well as the
+// base function name, the register it operates on, and whether it's a retain
+// function.
 static inline void foreachRRFunction(void (*call)(void (*)(void **regs), const char *name, int reg, int isRetain)) {
   #define CALL_ONE_FUNCTION(reg, function, isRetain) \
     call(call_ ## function ## _x ## reg, #function, reg, isRetain);
@@ -87,5 +106,4 @@ static inline void foreachRRFunction(void (*call)(void (*)(void **regs), const c
     FUNCTION_REGS(CALL_ONE_FUNCTION, function, isRetain)
 
   ALL_FUNCTIONS(CALL_WITH_FUNCTIONS)
-//   call_swift_retain_x21(0);
 }
--- test/Runtime/custom_rr_abi.swift
+++ test/Runtime/custom_rr_abi.swift
@@ -8,6 +8,10 @@
 
 import StdlibUnittest
 
+// A class that can provider a retainable pointer and determine whether it's
+// been retained or released. This creates a helper object that will be retained
+// or released. We don't attempt to clean up the helper so it leaks if not released,
+// but this is only used for this one test so that's OK.
 class RetainReleaseChecker {
   var pointerValue: UnsafeMutableRawPointer
 
@@ -19,17 +23,27 @@ class RetainReleaseChecker {
 
   init() {
     do {
+      // Make a helper object, retain it so it stays alive, and put it into
+      // pointerValue and weakRef.
       let helper = Helper()
       pointerValue = Unmanaged.passRetained(helper).toOpaque()
       weakRef = helper
     }
+    // Record the original retain count before anything happens. Then we can
+    // detect changes without needing to know exactly what the count is supposed
+    // to be.
     originalRetainCount = _getRetainCount(weakRef!)
   }
 
+  // If helper was retained, then weakRef will still point to it, and the retain
+  // count will have increased.
   var retained: Bool {
     weakRef != nil && _getRetainCount(weakRef!) > originalRetainCount
   }
 
+  // weakRef is the only reference we had to the helper, aside from the retain we put
+  // on it to create pointerValue. If helper was released, then it will be destroyed
+  // and weakRef will be nil.
   var released: Bool {
     weakRef == nil
   }
@@ -41,12 +55,19 @@ CustomRRABITestSuite.test("retain") {
   foreachRRFunction { function, cname, register, isRetain in
     let name = String(cString: cname!)
     let fullname = "\(name)_x\(register)"
-    print("Testing \(name)_x\(register)")
+
+    // Create a set of RR checker objects.
     var checkers = (0..<NUM_REGS).map{ _ in RetainReleaseChecker() }
+
+    // Fill out a registers array with the pointers from the RR checkers.
     var regs: [UnsafeMutableRawPointer?] = checkers.map{ $0.pointerValue }
 
+    // Call the RR function.
     function!(&regs)
 
+    // Make sure all the checkers report what they're supposed to. All registers
+    // aside from `register` should be untouched, and `register` should have been
+    // either retained or released.
     for (i, checker) in checkers.enumerated() {
       if i == register {
         if isRetain != 0 {
